393f5af609b2c582a2a4a4178d1164c616ff292e
AlinaZLY <1307204373@qq.com>
2024-11-30 12:47:30 +0800

    Part3
    For the Ride class, add the following methods: addVisitorToQueue(Visitor visitor)： Add the given visitor to the end of the visitor queue and print out the visitor's name ID、 Membership status and age information.
    removeVisitorFromQueue()： Remove the visitor at the head of the queue and print out the name of the removed visitor ID、 Membership status and age information. If the queue is empty, print a prompt message indicating that the queue is empty.
    printQueue()： Print out the names of all visitors in the current queue ID、 Membership status and age information. And in the partThree method of the AssignmentTwo class, first an employee rideOperaor1 and a ride ride1 are created, and then multiple visitor objects are created. Then, these visitors are added to the tourist queue by calling the addVisitorToQueue method of ride1, and a visitor in the queue is removed by calling the removeVisitorFromQueue method. Finally, the printQueue method is called to print all the visitor information in the current queue.

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,8 +1,32 @@
 public class AssignmentTwo {
     public static void main(String[] args) {
+        AssignmentTwo assignment = new AssignmentTwo();
+        assignment.partThree();
     }
+
     public void partThree(){
+        Employee rideOperator1 = new Employee("John Doe", "Male", 30, "001", "Operator");
+        Ride ride1 = new Ride("Roller Coaster", 2, rideOperator1);
+
+        Visitor visitor1 = new Visitor("Nehemia", "Female", 25, 1001, true);
+        Visitor visitor2 = new Visitor("Jason", "Male", 30, 1002, false);
+        Visitor visitor3 = new Visitor("Jack", "Male", 22, 1003, true);
+        Visitor visitor4 = new Visitor("Sharon", "Female", 27, 1004, false);
+        Visitor visitor5 = new Visitor("Benny", "Male", 35, 1005, true);
+        Visitor visitor6 = new Visitor("Leo", "Male", 17, 1006, false);
+
+        ride1.addVisitorToQueue(visitor1);
+        ride1.addVisitorToQueue(visitor2);
+        ride1.addVisitorToQueue(visitor3);
+        ride1.addVisitorToQueue(visitor4);
+        ride1.addVisitorToQueue(visitor5);
+        ride1.addVisitorToQueue(visitor6);
+
+        ride1.removeVisitorFromQueue();
+
+        ride1.printQueue();
     }
+
     public void partFourA(){
     }
     public void partFourB(){
--- src/Ride.java
+++ src/Ride.java
@@ -1,7 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Ride {
     private String rideName;
     private int maxPassengers;
     private Employee rideOperator;
+    private Queue<Visitor> visitorQueue;
+
 
     public Ride() {
         
@@ -11,12 +16,12 @@ public class Ride {
         this.rideName = rideName;
         this.maxPassengers = maxPassengers;
         this.rideOperator = rideOperator;
+        visitorQueue = new LinkedList<>();
     }
 
     public String getRideName() {
         return rideName;
     }
-
     public void setRideName(String rideName) {
         this.rideName = rideName;
     }
@@ -24,7 +29,6 @@ public class Ride {
     public int getMaxPassengers() {
         return maxPassengers;
     }
-
     public void setMaxPassengers(int maxPassengers) {
         this.maxPassengers = maxPassengers;
     }
@@ -32,8 +36,28 @@ public class Ride {
     public Employee getRideOperator() {
         return rideOperator;
     }
-
     public void setRideOperator(Employee rideOperator) {
         this.rideOperator = rideOperator;
+    }
+    
+    public void addVisitorToQueue(Visitor visitor) {
+        visitorQueue.add(visitor);
+        System.out.println(visitor.getName() + " has been added to the queue for ride " + rideName);
+    }
+    
+    public void removeVisitorFromQueue() {
+        Visitor visitor = visitorQueue.poll();
+        if (visitor != null) {
+            System.out.println(visitor.getName() + " has been removed from the queue for ride " + rideName);
+        } else {
+            System.out.println("Queue is empty. No visitor to remove.");
+        }
+    }
+    
+    public void printQueue() {
+        System.out.println("Visitors in the queue for ride " + rideName + ":");
+        for (Visitor visitor : visitorQueue) {
+            System.out.println(visitor.getName() + " (ID: " + visitor.getVisitorId() + ", Member: " + visitor.isMember() + ") - Age: " + visitor.getAge());
+        }
     }
 }
